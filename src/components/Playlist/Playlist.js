9f3e02168e9161b3bc24a1880ad304aabb8ab0d6
Yusuf Edmond <spoonfulltaken@gmail.com>
2023-08-05 11:42:42 -0400

    components

---
+++ src/components/Playlist/Playlist.js
@@ -0,0 +1,26 @@
+import React, { useCallback } from "react";
+
+import "./Playlist.css";
+
+import TrackList from "../TrackList/Tracklist"
+
+
+const Playlist = (props) => {
+    const handleNameChange = useCallback(
+        (event) => {
+            props.onNameChange(event.target.value)
+        },
+        [props.onNameChange]
+    )
+
+    return (
+        <div className="Playlist">
+            <input onChange={handleNameChange} defaultValue={"New Playlist"} />
+            <button className="Playlist-save" onClick={props.onSave}>
+                SAVE TO SPOTIFY
+            </button>
+        </div>
+    );
+};
+
+export default Playlist;
